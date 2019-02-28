65c9063abd46b4c0d181961cda6ff6157e6ffc5f
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2019-02-28 18:18:50 -0400

    first commit

---
+++ Scare/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.SimpleAudioPlayer;
+
+namespace Scare
+{
+    public partial class MainPage : ContentPage
+    {
+
+        int cont = 0;
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            var player = CrossSimpleAudioPlayer.Current;
+            player.Load("sequencia_de_tiros.mp3");
+
+            cont++;
+
+            label_mensagem.Text = "Vc Clicou " + cont + " vezes";
+
+            if (cont ==10)
+            {
+                player.Play();
+                label_mensagem.Text = "Buhhhh";
+                cont = 0;
+            }
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
